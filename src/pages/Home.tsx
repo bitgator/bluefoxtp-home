eec158f20e7c31f5eab7f70c188a71c44ec44c81
david-at-azurative <david.falkenberg@azurative.net>
2025-06-25 08:28:12 -0600

    add chat widget to Home
    adding chat widget to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react'
+
 export default function Home() {
+  useEffect(() => {
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = 'https://qa-crchat-cdn.azureedge.net/chatbox/chatstyleai.css'
+    document.head.appendChild(link)
+
+    const script = document.createElement('script')
+    script.src = 'https://qa-crchat-cdn.azureedge.net/chatbox/chatstyleai.js'
+    document.head.appendChild(script)
+
+    return () => {
+      document.head.removeChild(link)
+      document.head.removeChild(script)
+    }
+  }, [])
+
   return (
     <div className="min-h-screen flex flex-col items-center px-4" style={{backgroundColor: '#f0f0f0'}}>
       <div className="w-full max-w-5xl pt-32">
@@ -9,6 +27,12 @@ export default function Home() {
           Just testing things out here
         </h2>
       </div>
+      <chatstyleai-chatbox 
+        id="chatbox" 
+        channel="7f5f43b0-dfc9-4519-a79d-1b1a55de765b" 
+        version="" 
+        options='{"isVisible":"true", "isOpen":"false", "autoStart":"true", "env":"qa"}'
+      />
     </div>
   )
 }
