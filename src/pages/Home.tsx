6c56f57257d64b6e27fbb9dbdd0d7f18321625a0
david-at-azurative <david.falkenberg@azurative.net>
2025-11-12 17:07:43 -0700

    trying again to get the bot to show up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,22 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   const [scriptLoaded, setScriptLoaded] = useState(false)
 
-  useEffect(() => {
-    const link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.href = 'https://cdn.chatstyle.ai/chatbox/kendo/v20251112FIX/crchat-chatwidget.css'
-    document.body.appendChild(link)
+  // useEffect(() => {
+  //   const link = document.createElement('link')
+  //   link.rel = 'stylesheet'
+  //   link.href = 'https://cdn.chatstyle.ai/chatbox/kendo/v20251112FIX/crchat-chatwidget.css'
+  //   document.body.appendChild(link)
 
-    const script = document.createElement('script')
-    script.src = 'https://cdn.chatstyle.ai/chatbox/kendo/v20251112FIX/crchat-chatwidget.js'
-    script.onload = () => setScriptLoaded(true)
-    document.body.appendChild(script)
+  //   const script = document.createElement('script')
+  //   script.src = 'https://cdn.chatstyle.ai/chatbox/kendo/v20251112FIX/crchat-chatwidget.js'
+  //   script.onload = () => setScriptLoaded(true)
+  //   document.body.appendChild(script)
 
-    return () => {
-      // document.head.removeChild(link)
-      // document.head.removeChild(script)
-    }
-  }, [])
+  //   return () => {
+  //     // document.head.removeChild(link)
+  //     // document.head.removeChild(script)
+  //   }
+  // }, [])
 
   return (
     <div className="min-h-screen flex flex-col items-center px-4" style={{backgroundColor: '#f0f0f0'}}>
@@ -31,12 +31,12 @@ export default function Home() {
         </h2>
       </div>
 
-      <chatstyleai-chatbox
+      {/* <chatstyleai-chatbox
         id="chatbox"
         channel="1062790d-3b9f-4e15-b1cc-76883431ca7c"
         version=""
         options='{"isVisible":"true", "isOpen":"false", "autoStart":"true"}'
-      />
+      /> */}
     </div>
   )
 }
